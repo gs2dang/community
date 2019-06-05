ed40510ef111f6e4f4702380a5a2b86932bf4cb2
gs2dang <gs2dang07@gmail.com>
2019-06-05 19:17:56 +0900

    bin 폴더 수정

---
+++ .bin/docker.sh
@@ -0,0 +1,3 @@
+docker build -t $DOCKER_USERNAME/$IMAGE_REPO_NAME:test .
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+docker push $DOCKER_USERNAME/$IMAGE_REPO_NAME:test
