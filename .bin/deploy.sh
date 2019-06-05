60b88f6bd095eb142d326b5dd1a97325a29f18c0
gs2dang <gs2dang07@gmail.com>
2019-06-05 21:07:43 +0900

    기능-추가: .travis.yml, deploy.sh 추가
    Dev 환경으로 도커 빌드, 푸시 되는 것을 확인을 하고 ECS 관련 코드 추가를 했다.

---
+++ .bin/deploy.sh
@@ -0,0 +1,27 @@
+# install aws-sdk
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# install ecs-deploy
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
+  sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+# Use this for AWS ECR
+# eval $(aws ecr get-login --region us-east-1)
+
+# Use this for Docker Hub
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+
+# Build and push the image to the repository
+docker build -t $DOCKER_USERNAME/$IMAGE_REPO_NAME:testtest .
+#echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+docker tag $DOCKER_USERNAME/$IMAGE_REPO_NAME:latest $IMAGE_REPO_FULLNAME:latest
+docker push $DOCKER_USERNAME/$IMAGE_REPO_NAME:testtest
+
+# deploy the new image to the aws ecs
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_FULLNAME:latest
--- .bin/docker.sh
+++
@@ -1,3 +0,0 @@
-docker build -t $DOCKER_USERNAME/$IMAGE_REPO_NAME:test .
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-docker push $DOCKER_USERNAME/$IMAGE_REPO_NAME:test
