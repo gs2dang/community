afd8384944f8db3acf3fd3bfb74bc57a14991545
gs2dang <gs2dang07@gmail.com>
2019-06-07 23:07:24 +0900

    Travis 1차 테스트
    docker.sh 파일에서 도커 태그를 붙여줬다.

--- .bin/docker.sh
+++ .bin/docker.sh
@@ -1,3 +1,3 @@
-docker build -t $DOCKER_USERNAME/$REPO_NAME .
+docker build -t $DOCKER_USERNAME/$REPO_NAME:test .
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker push $DOCKER_USERNAME/$REPO_NAME:test
