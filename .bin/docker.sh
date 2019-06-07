c31457892f9c84bc7f1bb31b8fcf075d0fbf077e
gs2dang <gs2dang07@gmail.com>
2019-06-07 19:41:10 +0900

    테스트: docker.sh 도커 허브 로그인 코드 수정

--- .bin/docker.sh
+++ .bin/docker.sh
@@ -1,10 +1,5 @@
-# Use this for Docker Hub
-docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
-echo "DockerHub login success"
-
 # Build and push the image to the repository
 docker build -t $DOCKER_USERNAME/$IMAGE_REPO_NAME .
-echo "Docker build success"
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker tag $DOCKER_USERNAME/$IMAGE_REPO_NAME:test $IMAGE_REPO_FULLNAME:test
 docker push $DOCKER_USERNAME/$IMAGE_REPO_NAME:test
-echo "Docker push success"
