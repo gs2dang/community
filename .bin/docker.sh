4deefdbc8417ee2bbd0eb227f5e06a91037f73d2
gs2dang <gs2dang07@gmail.com>
2019-06-07 19:58:15 +0900

    테스트: docker.sh 빌드, 푸시 코드 수정

--- .bin/docker.sh
+++ .bin/docker.sh
@@ -1,5 +1,3 @@
-# Build and push the image to the repository
 docker build -t $DOCKER_USERNAME/$IMAGE_REPO_NAME .
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-docker tag $DOCKER_USERNAME/$IMAGE_REPO_NAME:test $IMAGE_REPO_FULLNAME:test
 docker push $DOCKER_USERNAME/$IMAGE_REPO_NAME:test
